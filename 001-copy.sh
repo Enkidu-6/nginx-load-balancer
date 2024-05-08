eb7e142ffb6b751ee95554e49db7c22aa6e49214
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2024-05-08 01:53:28 -0400

    Update 001-copy.sh

--- 001-copy.sh
+++ 001-copy.sh
@@ -2,8 +2,8 @@
 
 echo "SSL certificates renewed"
 
-/bin/cp -Lrf /etc/letsencrypt/live/3x2.wcbsecurity.com/fullchain.pem /root/certs/fullchain.pem
-/bin/cp -Lrf /etc/letsencrypt/live/3x2.wcbsecurity.com/privkey.pem /root/certs/privkey.pem
+/bin/cp -Lrf /etc/letsencrypt/live/DOMAIN.TLD/fullchain.pem /root/certs/fullchain.pem
+/bin/cp -Lrf /etc/letsencrypt/live/DOMAIN.TLD/privkey.pem /root/certs/privkey.pem
 docker compose -f /root/docker-compose.yml start
 
 echo "SSL certificates copied"
@@ -11,4 +11,4 @@ echo "docker started"
 
 # Place this file in /etc/letsencrypt/renewal-hooks/post/ and chmod to 0700
 # It will copy the new certificates to /root/certs/  automatically after each renewal
-# and restart your docker containers
+# and restarts your docker containers
